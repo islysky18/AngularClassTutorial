a3bab3e79595460db8d5e42012558c316644ac3c
Chao-Ting Wen <tobias_wenct@gmail.com>
2021-03-13 13:28:12 -0500

    change server to webapi

--- class-app/src/app/services/mytest.service.ts
+++ class-app/src/app/services/mytest.service.ts
@@ -11,7 +11,19 @@ export class MytestService {
   ) {}
 
   getData() {
-    return this.httpClient.get('assets/data.json'); ///call to backend
+    return this.httpClient.get('https://localhost:44326/api/Quote'); ///call to backend
+  }
+
+  postData(item) {
+    return this.httpClient.post('', item);
+  }
+
+  deleData() {
+    return this.httpClient.delete('api/values/4');
+  }
+
+  updateData() {
+    return this.httpClient.put('', {});
   }
 
   sayHello() {
